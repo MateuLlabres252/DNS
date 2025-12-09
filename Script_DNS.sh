8c0e35462434485e87adef0951d0591c67e158d9
MateuLlabres252 <mateullabres@paucasesnovescifp.cat>
2025-12-09 19:57:08 +0100

    Add files via upload

---
+++ Script_DNS.sh
@@ -0,0 +1,15 @@
+# Actualitzar paquets i instal  lar el servei DNS
+apt update
+apt upgrade -y
+apt install bind9 -y
+
+# Connexi   amb GitHub
+BASE_URL="https://github.com/PauAlumnesS2I/sxi-ci-g03.git"
+
+curl -sSL "$BASE_URL/named.conf.local" -o "/etc/bind/named.conf.local"
+curl -sSL "$BASE_URL/db.granalmacen.org" -o "/etc/bind/db.granalmacen.org"
+curl -sSL "$BASE_URL/db.xarxaadmin.granalmacen.org" -o "/etc/bind/db.xarxaadmin.granalmacen.org"
+curl -sSL "$BASE_URL/db.inversa" -o "/etc/bind/db.inversa"
+# Reiniciar el servei
+systemctl restart bind9
+systemctl status bind9
