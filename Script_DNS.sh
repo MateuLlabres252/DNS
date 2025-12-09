ce6fd4a9d60c8322007440c7689e86b999db0e05
MateuLlabres252 <mateullabres@paucasesnovescifp.cat>
2025-12-09 20:33:08 +0100

    Update Script_DNS.sh to clone DNS repo and copy files
    Added commands to clone a DNS repository and copy configuration files.

--- Script_DNS.sh
+++ Script_DNS.sh
@@ -2,6 +2,12 @@
 apt update
 apt upgrade -y
 apt install bind9 git -y
+
+# Descargar el repositorio completo en una carpeta temporal
+# (Usamos git clone porque la URL termina en .git)
+rm -rf /tmp/DNS_temp
+git clone https://github.com/MateuLlabres252/DNS.git /tmp/DNS_temp
+
 # Copiar los archivos a la ruta final
 cp /tmp/DNS_temp/named.conf.local /etc/bind/
 cp /tmp/DNS_temp/db.granalmacen.org /etc/bind/
